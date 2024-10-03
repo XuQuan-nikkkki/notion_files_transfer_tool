c461ddd2eda5a2ce254f770eacf24ff00d16377f
Xu Quan <xuquan1225@hotmail.com>
2024-10-03 18:02:18 +0800

    feat: integrate Ant Design as the primary component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { CSSProperties } from "react";
+import { Layout } from "antd";
+
+const { Header } = Layout;
+
+const layoutStyle: CSSProperties = {
+  height: "100vh",
+};
+
+const headerStyle: CSSProperties = {
+  color: "#fff",
+  fontSize: 18,
+  fontWeight: 800,
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
 
 const App = () => {
   return (
     <div className="app">
-      Notion Files Transfer Tool
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>Notion Files Transfer Tool</Header>
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
