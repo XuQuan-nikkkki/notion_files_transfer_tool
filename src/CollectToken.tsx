4fbc6549a024b7f2540be984a003c4b5e6b3bd57
Xu Quan <xuquan1225@hotmail.com>
2024-10-16 23:01:00 +0800

    feat: add instructions for collecting token

--- src/CollectToken.tsx
+++ src/CollectToken.tsx
@@ -1,7 +1,8 @@
-import { Input, Form, Button, Space } from "antd";
-import React from "react";
+import { Input, Form, Button, Space, Divider } from "antd";
+import React, { CSSProperties } from "react";
 
 import { CollectTokenActionCopy, Language } from "./constant";
+import VerticalSpace from "./VerticalSpace";
 
 const { useForm, Item: FormItem } = Form;
 
@@ -20,8 +21,208 @@ const CollectToken = ({ language }: Props) => {
     form.setFieldsValue({ token: "" });
   };
 
+  const renderInstruction = () => {
+    const imageStyle: CSSProperties = {
+      padding: "10px 0",
+    };
+
+    if (language === "English") {
+      return (
+        <section>
+          <h3>Instructions</h3>
+          <ol>
+            <li>
+              Visit Notion's{" "}
+              <a
+                href="https://www.notion.so/profile/integrations"
+                target="_blank"
+              >
+                My Creator Profile
+              </a>
+              .
+            </li>
+            <li>If not logged in, please log into your Notion account.</li>
+            <li>
+              Click the "+ New Integration" button.
+              <br />
+              <img
+                alt="add new integration"
+                width={500}
+                src="../assets/add_new_integration.jpg"
+                style={imageStyle}
+              />
+            </li>
+            <li>
+              Fill in the integration details:
+              <ol>
+                <li>Name the integration, i.e. Files Transfer Tool.</li>
+                <li>Select the appropriate Workspace.</li>
+                <li>Choose "Internal" as the Type.</li>
+                <li>Click "Save" after completing these fields.</li>
+              </ol>
+              <img
+                alt="fill in details"
+                src="../assets/fill_in_details.jpg"
+                width={800}
+                style={imageStyle}
+              />
+            </li>
+            <li>
+              A popup will appear saying "Integration successfully created",
+              with a button labeled "Configure integration settings". Click this
+              button.
+              <br />
+              <img
+                alt="integration created popup"
+                width={300}
+                src="../assets/integration_created_popup.jpg"
+                style={imageStyle}
+              />
+            </li>
+            <li>
+              On the integration settings page, ensure that "Read content",
+              "Update content", and "Insert content" are checked by default and
+              leave them unchanged.
+              <br />
+              <img
+                alt="remain capabilities unchanged"
+                width={800}
+                src="../assets/capabilities.jpg"
+                style={imageStyle}
+              />
+            </li>
+            <li>
+              Copy integration secret:
+              <ol>
+                <li>
+                  Click the "Show" button next to the "Internal Integration
+                  Secret" input field to reveal the Secret.
+                  <br />
+                  <img
+                    alt="click show secret"
+                    width={800}
+                    src="../assets/show_secret.jpg"
+                    style={imageStyle}
+                  />
+                </li>
+                <li>
+                  Once shown, the "Show" button will turn into a "Copy" button.
+                  Click "Copy" to copy the Secret.
+                  <br />
+                  <img
+                    alt="copy the secret"
+                    width={800}
+                    src="../assets/copy_secret.jpg"
+                    style={imageStyle}
+                  />
+                </li>
+              </ol>
+            </li>
+            <li>Paste the copied Secret into the input below.</li>
+          </ol>
+        </section>
+      );
+    }
+
+    return (
+      <section>
+        <h3>操作指南</h3>
+        <ol>
+          <li>
+            访问 Notion 的{" "}
+            <a
+              href="https://www.notion.so/profile/integrations"
+              target="_blank"
+            >
+              My Creator Profile 页面
+            </a>
+            ；
+          </li>
+          <li>如未登录，需先登录 Notion 账户；</li>
+          <li>
+            点击页面中的「+ New Integration」；
+            <br />
+            <img
+              alt="add new integration"
+              width={500}
+              src="../assets/add_new_integration.jpg"
+              style={imageStyle}
+            />
+          </li>
+          <li>
+            补充 Integration 的信息：
+            <ol>
+              <li>为 Integration 命名；</li>
+              <li>选择相应的 Workspace；</li>
+              <li>
+                Type 选择 <code>Internal</code>；
+              </li>
+              <li>完成后点击 "Save"；</li>
+            </ol>
+            <img
+              alt="fill in details"
+              src="../assets/fill_in_details.jpg"
+              width={800}
+              style={imageStyle}
+            />
+          </li>
+          <li>
+            此时会弹出提示框，显示 Integration 创建成功（Integration
+            successfully created），并显示配置按钮（Configure integration
+            settings），点击该按钮
+            <br />
+            <img
+              alt="integration created popup"
+              width={300}
+              src="../assets/integration_created_popup.jpg"
+              style={imageStyle}
+            />
+          </li>
+          <li>
+            进入集成配置页面，默认勾选的 "Read content"、"Update content" 和
+            "Insert content" 不需要修改；
+            <br />
+            <img
+              alt="remain capabilities unchanged"
+              width={800}
+              src="../assets/capabilities.jpg"
+              style={imageStyle}
+            />
+          </li>
+          <li>
+            复制 Integration Secret：
+            <ol>
+              <li>
+                点击 Internal Integration Secret 输入框后的 Show；
+                <br />
+                <img
+                  alt="click show secret"
+                  width={800}
+                  src="../assets/show_secret.jpg"
+                  style={imageStyle}
+                />
+              </li>
+              <li>
+                Secret 展示后，Show 按钮会变成 Copy。点击 Copy 复制 secret；
+                <br />
+                <img
+                  alt="copy the secret"
+                  width={800}
+                  src="../assets/copy_secret.jpg"
+                  style={imageStyle}
+                />
+              </li>
+            </ol>
+          </li>
+          <li>将复制的 Secret 粘贴到下方的输入框中。</li>
+        </ol>
+      </section>
+    );
+  };
+
   return (
-    <div>
+    <VerticalSpace split={<Divider />}>
+      {renderInstruction()}
       <Form form={form}>
         <FormItem name="token" label="Token:" rules={[{ required: true }]}>
           <Input style={{ width: 400 }} />
@@ -37,7 +238,7 @@ const CollectToken = ({ language }: Props) => {
           </Space>
         </FormItem>
       </Form>
-    </div>
+    </VerticalSpace>
   );
 };
 
